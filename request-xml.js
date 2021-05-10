0cea7642e630fb6d25c6f6cf995befbf1a2ed5db
Hibana <pagliaccizeus@gmail.com>
2021-05-10 17:16:16 +0200

    add sort table for request.xml page

--- request-xml.js
+++ request-xml.js
@@ -14,6 +14,8 @@ function fetchingData(search) {
       }
 
       checkerSearchData(xmlDoc);
+
+      sortTableData(xmlDoc);
     });
 }
 
@@ -135,4 +137,104 @@ function createXmlTable(data) {
   }
 
   document.querySelector("table").innerHTML = html;
+}
+
+function sortTableData(data) {
+  const convertedXml = xmlToJson(data);
+
+  const arrayOfXmlTags = convertedXml.CATALOG.CD;
+
+  // console.log(arrayOfXmlTags);
+
+  document.querySelector(".tableInfo").addEventListener("click", (event) => {
+    if (event.target.nodeName !== "TH") {
+      return;
+    }
+
+    let nameToSearch = event.target.innerText.toUpperCase();
+    console.log(nameToSearch);
+    const sortedArr = arrayOfXmlTags.sort((a, b) =>
+      sortUniversal(a, b, nameToSearch)
+    );
+
+    createSortedTable(sortedArr);
+  });
+}
+
+function createSortedTable(arr) {
+  let html = ``;
+  html += ` <tr>`;
+  html += ` <th>Title</th>`;
+  html += `<th>Artist</th>`;
+  html += ` <th>Country</th>`;
+  html += ` <th>Company</th>`;
+  html += `<th>Price</th>`;
+  html += `<th>Year</th>`;
+  html += `</tr>`;
+
+  arr.forEach(({ TITLE, ARTIST, COUNTRY, COMPANY, PRICE, YEAR }) => {
+    html += `<tr>`;
+    html += `<td> ${TITLE}</td>`;
+    html += `<td> ${ARTIST}</td>`;
+    html += `<td> ${COUNTRY}</td>`;
+    html += `<td> ${COMPANY}</td>`;
+    html += `<td> ${PRICE}</td>`;
+    html += `<td> ${YEAR}</td>`;
+    html += `</tr>`;
+  });
+
+  document.querySelector("table").innerHTML = html;
+}
+
+// https://gist.github.com/chinchang/8106a82c56ad007e27b1 link to function of github
+function xmlToJson(xml) {
+  var obj = {};
+
+  if (xml.nodeType == 1) {
+    if (xml.attributes.length > 0) {
+      obj["@attributes"] = {};
+      for (let j = 0; j < xml.attributes.length; j++) {
+        let attribute = xml.attributes.item(j);
+        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+  } else if (xml.nodeType == 3) {
+    obj = xml.nodeValue;
+  }
+  let textNodes = [].slice.call(xml.childNodes).filter(function (node) {
+    return node.nodeType === 3;
+  });
+  if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
+    obj = [].slice.call(xml.childNodes).reduce(function (text, node) {
+      return text + node.nodeValue;
+    }, "");
+  } else if (xml.hasChildNodes()) {
+    for (var i = 0; i < xml.childNodes.length; i++) {
+      let item = xml.childNodes.item(i);
+      let nodeName = item.nodeName;
+      if (typeof obj[nodeName] == "undefined") {
+        obj[nodeName] = xmlToJson(item);
+      } else {
+        if (typeof obj[nodeName].push == "undefined") {
+          let old = obj[nodeName];
+          obj[nodeName] = [];
+          obj[nodeName].push(old);
+        }
+        obj[nodeName].push(xmlToJson(item));
+      }
+    }
+  }
+  return obj;
+}
+
+function sortUniversal(a, b, value) {
+  if (a[value] < b[value]) {
+    return -1;
+  }
+
+  if (a[value] > b[value]) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
